bf30af477de78ae97b85dfc4c27f68ea181ed3e6
Mark D <markd@Marks-MacBook-Pro.local>
2020-08-06 08:21:42 -0400

    initial xcode project creation

---
+++ youtube-otb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-otb
+//
+//  Created by Mark D on 8/6/20.
+//  Copyright © 2020 MAD Endeavors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
