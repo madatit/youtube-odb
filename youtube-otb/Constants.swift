f6cccd19d0304342bf671a13997e222b85caa831
Mark D <markd@Marks-MacBook-Pro.local>
2020-08-07 09:53:34 -0400

    test

---
+++ youtube-otb/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  youtube-otb
+//
+//  Created by Mark D on 8/6/20.
+//  Copyright © 2020 MAD Endeavors. All rights reserved.
+//
+
+import Foundation
+// Not loading
+
+struct Constants {
+    
+    static var API_KEY = "AIzaSyBQQso1N9THB9D4kFRTsqo80LXKUiwrYcE"
+    static var PLAYLIST_ID = "PLMRqhzcHGw1YSKIO61XncxTPoFu81K1Mx"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var test = ""
+    
+    
+}
